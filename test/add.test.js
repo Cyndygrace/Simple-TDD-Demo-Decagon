91afc795ef6800b9555dcec66493e728a84fec22
Cynthia Iloekwe <cyniloekwe@gmail.com>
2019-04-12 11:38:05 +0100

    added another test to test the add.js code which passed

--- test/add.test.js
+++ test/add.test.js
@@ -5,3 +5,9 @@ test("that 2 and 3 and 4 and 5 will output 14", () =>{
     expect(add([2, 3, 4, 5])).toEqual(14)
 
 });
+
+test("that 5 and 10 and 50 and 15 will output 80", () =>{
+
+    expect(add([5, 10, 50, 15])).toEqual(80)
+
+});
